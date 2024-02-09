f936b6a77093374814749351b95f7178237421cd
Harapanee <harasum111@gmail.com>
2024-02-09 18:49:25 +0900

    add StatefulWidget
    ランダムに表示される英単語をStatefulに変更

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,21 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
